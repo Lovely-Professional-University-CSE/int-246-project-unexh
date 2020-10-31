cef2096eb023bebb2a74cd6187ee3cd5746e4813
Nishant Pandey <unexme21@gmail.com>
2020-10-31 21:35:28 +0530

    Update interface.py
    Changed gene_count min to 2 and reduced some loc

--- interface.py
+++ interface.py
@@ -2,23 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, DecimalField, SubmitField
 from wtforms.validators import DataRequired, NumberRange, optional
 
-minn = 0
-maxx = 1000
+minn, maxx = 0, 1000
 
 class GA_Interface(FlaskForm):
-    weight1 = IntegerField('Weights', validators=[DataRequired(), NumberRange(min=1,max=maxx)])
-    weight2 = IntegerField('Weights', validators=[optional(), NumberRange(min=minn,max=maxx)])
-    weight3 = IntegerField('Weights', validators=[optional(), NumberRange(min=minn,max=maxx)])
-    weight4 = IntegerField('Weights', validators=[optional(), NumberRange(min=minn,max=maxx)])
+    weight1 = IntegerField('Weights', validators=[DataRequired(), NumberRange(min=1, max=maxx)])
+    weight2 = weight3 = weight4 = IntegerField('Weights', validators=[optional(), NumberRange(min=minn, max=maxx)])
     
-    value1 = IntegerField('Values', validators=[DataRequired(),NumberRange(min=1,max=maxx)])
-    value2 = IntegerField('Values', validators=[optional(), NumberRange(min=minn,max=maxx)])
-    value3 = IntegerField('Values', validators=[optional(), NumberRange(min=minn,max=maxx)])
-    value4 = IntegerField('Values', validators=[optional(), NumberRange(min=minn,max=maxx)])
+    value1 = IntegerField('Values (Profit)', validators=[DataRequired(),NumberRange(min=1, max=maxx)])
+    value2 = value3 = value4 = IntegerField('Values (Profit)', validators=[optional(), NumberRange(min=minn, max=maxx)])
 
-    knapsack_value   = IntegerField('Knapsack Value', validators=[DataRequired(),NumberRange(min=1,max=maxx)])
-    generation_count = IntegerField('Net Generations', validators=[optional(), NumberRange(min=1,max=maxx)])
-    gene_count       = IntegerField('Chromosome Length', validators=[optional(), NumberRange(min=1,max=maxx)])
-    mutation_rate    = DecimalField('Mutation Rate', validators=[optional(), NumberRange(min=0.001,max=1.0)])
-    crossover_rate   = DecimalField('Crossover Rate', validators=[optional(), NumberRange(min=0.001,max=1.0)])
-    submit           = SubmitField('Get Results')
+    knapsack_value      = IntegerField('Knapsack Value', validators=[DataRequired(), NumberRange(min=1, max=maxx)])
+    generation_count    = IntegerField('Net Generations', validators=[optional(), NumberRange(min=1, max=maxx)])
+    gene_count          = IntegerField('Chromosome Length', validators=[optional(), NumberRange(min=2, max=maxx)])
+    mutation_rate       = DecimalField('Mutation Rate', validators=[optional(), NumberRange(min=0.001, max=1.0)])
+    crossover_rate      = DecimalField('Crossover Rate', validators=[optional(), NumberRange(min=0.001, max=1.0)])
+    submit              = SubmitField('Get Results')
